f8d378a8c0f4d7f72b7e79b25f69bee56e2506b9
Ben Barsdell <bbarsdell@nvidia.com>
2021-02-10 13:55:28 +1100

    Remove erroneous semicolon from FLT_MIN definition

--- jitify2.hpp
+++ jitify2.hpp
@@ -3004,7 +3004,7 @@ JITIFY_DEFINE_C_AND_CXX_HEADERS(float, R"(
 #define DBL_MAX         1.7976931348623157e308
 #define FLT_EPSILON     1.19209289e-7f
 #define DBL_EPSILON     2.220440492503130e-16
-#define FLT_MIN         1.1754943e-38f;
+#define FLT_MIN         1.1754943e-38f
 #define DBL_MIN         2.2250738585072013e-308
 #define FLT_ROUNDS      1
 #if defined __cplusplus && __cplusplus >= 201103L
